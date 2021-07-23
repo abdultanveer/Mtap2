9ddd3fbc8790027316299d7213b794dd23e03cc4
abdul <abdul.tanveer@gmail.com>
2021-07-23 15:07:02 +0530

    Initial commit

---
+++ app/src/main/java/com/abdul/mtap2/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.abdul.mtap2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
