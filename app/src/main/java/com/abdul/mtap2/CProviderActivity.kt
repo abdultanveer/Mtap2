ac06e1d1888662a46ef4aef1c76722750da9ca53
abdul <abdul.tanveer@gmail.com>
2021-07-28 13:06:32 +0530

    runtime permissions

--- app/src/main/java/com/abdul/mtap2/CProviderActivity.kt
+++ app/src/main/java/com/abdul/mtap2/CProviderActivity.kt
@@ -1,10 +1,14 @@
 package com.abdul.mtap2
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.ListView
 import android.widget.SimpleCursorAdapter
 import androidx.appcompat.app.AppCompatActivity
+
 
 class CProviderActivity : AppCompatActivity() {
 
@@ -17,6 +21,13 @@ class CProviderActivity : AppCompatActivity() {
         var fromColumn = arrayOf("body","address")
         var toTextView = intArrayOf(android.R.id.text1,android.R.id.text2)
         var rowLayout = android.R.layout.simple_list_item_2  //C:\Users\Admin\AppData\Local\Android\Sdk\platforms\android-28\data\res\layout
+
+        if(!checkReadSMSPermission()){
+          //  onPermissionCallBack = this
+            getReadSMSPermission();
+        }
+
+
         var dataCursor =
             contentResolver.query(uriSms,null,null,null,null,null)
 
@@ -24,4 +35,50 @@ class CProviderActivity : AppCompatActivity() {
             SimpleCursorAdapter(this,rowLayout,dataCursor,fromColumn,toTextView,1)
         cpListview.adapter =adapter
     }
+
+
+    private fun checkReadSMSPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) {
+                true
+            } else {
+                false
+            }
+        } else {
+            true
+        }
+    }
+ var REQUEST_READ_SMS_PERMISSION = 123
+    var onPermissionCallBack: RequestPermissionAction? = null
+
+    fun getReadSMSPermission() {
+        //onPermissionCallBack = onPermissionCallBack
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!checkReadSMSPermission()) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_SMS),
+                    REQUEST_READ_SMS_PERMISSION
+                )
+                return
+            }
+        }
+        //if (onPermissionCallBack != null) onPermissionCallBack.permissionGranted()
+    }
+
+
+    //fun getReadSmsPermission(){}
+    open interface RequestPermissionAction {
+        fun permissionDenied()
+        fun permissionGranted()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    }
+
 }
