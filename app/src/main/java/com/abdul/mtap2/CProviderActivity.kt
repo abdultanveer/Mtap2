61ad85701ccaf633d89956df7b27e54d8165d645
abdul <abdul.tanveer@gmail.com>
2021-07-28 11:43:08 +0530

    content providers -- sms inbox - added phone no textview to each row

--- app/src/main/java/com/abdul/mtap2/CProviderActivity.kt
+++ app/src/main/java/com/abdul/mtap2/CProviderActivity.kt
@@ -14,14 +14,14 @@ class CProviderActivity : AppCompatActivity() {
         setContentView(R.layout.activity_cprovider)
         cpListview = findViewById(R.id.cpListView)
         val uriSms: Uri = Uri.parse("content://sms/inbox")
-        var fromColumn = arrayOf("body")
-        var toTextView = intArrayOf(android.R.id.text1)
-        var rowLayout = android.R.layout.simple_list_item_1
+        var fromColumn = arrayOf("body","address")
+        var toTextView = intArrayOf(android.R.id.text1,android.R.id.text2)
+        var rowLayout = android.R.layout.simple_list_item_2  //C:\Users\Admin\AppData\Local\Android\Sdk\platforms\android-28\data\res\layout
         var dataCursor =
             contentResolver.query(uriSms,null,null,null,null,null)
 
         var adapter:SimpleCursorAdapter =
-            SimpleCursorAdapter(this,rowLayout,dataCursor,fromColumn,toTextView,)
+            SimpleCursorAdapter(this,rowLayout,dataCursor,fromColumn,toTextView,1)
         cpListview.adapter =adapter
     }
 }
